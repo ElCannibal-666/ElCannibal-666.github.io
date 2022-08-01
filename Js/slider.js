640da4ef1d2ac7081be648ba2c8edbccf44072dd
ElCannibal-666 <super_pollo97@hotmail.com>
2022-08-01 13:25:43 -0500

    actualizacion

--- Js/slider.js
+++ Js/slider.js
@@ -1,30 +1,30 @@
-(function(){
-    
+(function () {
+
     const sliders = [...document.querySelectorAll('.works_body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
-    let value;   
+    let value;
 
-    buttonNext.addEventListener('click', ()=>{
+    buttonNext.addEventListener('click', () => {
         changePosition(1);
     });
 
-    buttonBefore.addEventListener('click', ()=>{
+    buttonBefore.addEventListener('click', () => {
         changePosition(-1);
     });
 
-    const changePosition = (add)=>{
+    const changePosition = (add) => {
         const currentTestimony = document.querySelector('.works_body--show').dataset.id;
         value = Number(currentTestimony);
-        value+= add;
+        value += add;
 
 
-        sliders[Number(currentTestimony)-1].classList.remove('works_body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
+        sliders[Number(currentTestimony) - 1].classList.remove('works_body--show');
+        if (value === sliders.length + 1 || value === 0) {
+            value = value === 0 ? sliders.length : 1;
         }
 
-        sliders[value-1].classList.add('works_body--show');
+        sliders[value - 1].classList.add('works_body--show');
 
     }
 
